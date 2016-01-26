173732ebe0ef6a999b7198d70c9ae0e58b482b4c
Alexey Dobrushskiy <alexey.dobrushskiy@revelsystems.com>
2016-01-26 17:19:58 +0400

    Arcanoid: semi-working

--- lesson4/arcanoid/index.js
+++ lesson4/arcanoid/index.js
@@ -19,22 +19,32 @@ var CENTER_X;
 var CENTER_Y;
 var BRICKS = [];
 var RADIUS = 15;
+var DIAMETER = RADIUS*2;
 var TIMER;
 var STATE = 1; // 1 - Running, 0 - paused
+var PLATFORM_LEFT = 0;
+var PLATFORM_WIDTH = 70;
+var PLATFORM_SPEED = 15;
+
+var KEY_CODE_LEFT_ARROW = 37;
+var KEY_CODE_RIGHT_ARROW = 39;
+var KEY_CODE_SPACE = 32;
+var RUNNING = true;
 
 function change_coords() {
     // 'ball' should be an element with 'position' = 'absolute'
     if ((TOP_VALUE >= Y_BOTTOM) || (TOP_VALUE <= Y_TOP)) {
-        Y_SPEED = -Y_SPEED;
+        if ((LEFT_VALUE>=PLATFORM_LEFT-DIAMETER) && (LEFT_VALUE<=PLATFORM_LEFT+PLATFORM_WIDTH)) {
+            Y_SPEED = -Y_SPEED;
+        } else {
+            RUNNING = false;
+            game_over();
+        }
     }
     if ((LEFT_VALUE >= X_RIGHT) || (LEFT_VALUE <= X_LEFT)) {
         X_SPEED = -X_SPEED;
     }
     for (var i=0; i<BRICKS.length; i++) {
-        //if (TOP_VALUE < 60) {
-        //    debugger;
-        //}
-
         var brick = BRICKS[i];
         if ((LEFT_VALUE >= brick.left) && (LEFT_VALUE <= brick.right) && (TOP_VALUE >= brick.top) && (TOP_VALUE <= brick.bottom)) {
             Y_SPEED = -Y_SPEED;
@@ -77,14 +87,61 @@ function fill_bricks() {
     }
 }
 
-window.onload = function() {
+function move_platform_left() {
+    if (PLATFORM_LEFT>X_LEFT) {
+        PLATFORM_LEFT -= PLATFORM_SPEED;
+        window.PLATFORM.style.left = PLATFORM_LEFT+'px';
+
+    }
+}
+
+function move_platform_right() {
+    if (PLATFORM_LEFT+PLATFORM_WIDTH<X_RIGHT+DIAMETER) {
+        PLATFORM_LEFT += PLATFORM_SPEED;
+        window.PLATFORM.style.left = PLATFORM_LEFT+'px';
+    }
+}
+
+function initialize(){
     window.BALL = document.getElementById('id_ball');
+    window.PLATFORM = document.getElementById('id_platform');
+    PLATFORM_LEFT = X_LEFT;
+    PLATFORM.style.left = PLATFORM_LEFT + 'px';
+    PLATFORM.style.top = Y_BOTTOM+30 + 'px';
     BALL.style.top = Y_BOTTOM + 'px';
     BALL.style.left = X_LEFT + 'px';
     fill_bricks();
     TIMER = setInterval(change_coords, 1);
+    STATE = 1;
+    RUNNING = true;
+    TOP_VALUE = Y_BOTTOM;
+    LEFT_VALUE = X_LEFT+1;
+}
+
+function game_over() {
+    clearInterval(TIMER);
+}
+
+window.onload = function() {
+    initialize();
 };
 
+// Events assigning
+window.onkeydown = function (ev) {
+    if (ev.keyCode == KEY_CODE_LEFT_ARROW) {
+        move_platform_left();
+    }
+    if (ev.keyCode == KEY_CODE_RIGHT_ARROW) {
+        move_platform_right();
+    }
+    if (ev.keyCode == KEY_CODE_SPACE) {
+        initialize();
+    }
+
+};
+
+
+// Pause action on click
 window.onclick = function() {
     if (STATE==1) {
         clearInterval(TIMER);
@@ -93,5 +150,4 @@ window.onclick = function() {
         TIMER = setInterval(change_coords, 1);
         STATE = 1;
     }
-
 };
